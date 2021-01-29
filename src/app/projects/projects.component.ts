fe16f96953ee3cd419eb739de894648b8fc54a5d
aman1210 <aman.srivastava101@gmail.com>
2021-01-29 13:00:17 +0530

    adding more projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -15,7 +15,16 @@ export class ProjectsComponent implements OnInit {
       'Android',
       'An interactive app to manage personal expenses.',
       'Flutter and Android Studio',
-      'https://github.com/aman1210/FlutterExpenseTracker'
+      'https://github.com/aman1210/FlutterExpenseTracker',
+      'https://play.google.com/store/apps/details?id=com.souleater.expenseTracker'
+    ),
+    new Project(
+      'SoulMusic',
+      '../../assets/p2.png',
+      'Web app',
+      'A simple music streaming web app.',
+      'Angular, NodeJS, Express and MongoDB',
+      'https://github.com/aman1210/MusicProject'
     ),
   ];
   constructor() {}
