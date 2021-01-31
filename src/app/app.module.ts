9003e01beb2a9e5f26415ac5458d2d742184f245
aman1210 <aman.srivastava101@gmail.com>
2021-01-31 16:03:17 +0530

    updating contact and generating deployables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { InfiniteTypeDeleteModule } from 'ngx-sbz-type-delete';
 import { NgScrollbarModule } from 'ngx-scrollbar';
 
-import { ParallaxDirective } from './parallax.directive';
-
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
 import { AppBarComponent } from './app-bar/app-bar.component';
@@ -12,24 +10,17 @@ import { FooterComponent } from './footer/footer.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ScrollService } from './scroll.service';
-import { AppRoute } from './app-route.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParallaxDirective,
     IntroComponent,
     AppBarComponent,
     FooterComponent,
     SkillsComponent,
     ProjectsComponent,
   ],
-  imports: [
-    BrowserModule,
-    InfiniteTypeDeleteModule,
-    NgScrollbarModule,
-    AppRoute,
-  ],
+  imports: [BrowserModule, InfiniteTypeDeleteModule, NgScrollbarModule],
   providers: [ScrollService],
   bootstrap: [AppComponent],
 })
